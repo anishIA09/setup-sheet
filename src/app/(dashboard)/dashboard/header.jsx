5225ae653280f0870a391455e5caecde82a49e4a
anshIA09 <Anish.Pawaskar@interactiveavenues.com>
2025-11-24 13:24:56 +0530

    style: reduce download button height and make stats card responsive

--- src/app/(dashboard)/dashboard/header.jsx
+++ src/app/(dashboard)/dashboard/header.jsx
@@ -31,17 +31,17 @@ export const DashboardHeader = () => {
     <>
       <div className="flex items-center justify-between">
         <h3 className="text-2xl font-semibold">Dashboard</h3>
-        <button className="h-10 gap-2 text-sm font-semibold rounded-md bg-primary text-primary-foreground px-3 py-2 flex items-center justify-center">
+        <button className="h-9 gap-2 text-sm font-semibold rounded-md bg-primary text-primary-foreground px-3 py-2 flex items-center justify-center">
           <DownloadIcon size={18} />
           <span>Download Your Plan</span>
         </button>
       </div>
-      <div className="flex items-center gap-4 flex-wrap">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {STATS.map((stat) => {
           return (
             <div
               key={stat.label}
-              className="p-4 rounded-md bg-card text-card-foreground flex items-center justify-between shadow-md font-semibold gap-3 max-w-[25%] w-full"
+              className="p-4 rounded-md bg-card text-card-foreground flex items-center justify-between shadow-md font-semibold gap-3"
             >
               <div className="flex flex-col gap-1">
                 <p className="text-primary uppercase tracking-tight text-sm">
